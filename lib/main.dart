ab27daac3fe054279c752041df0c53577c4040c6
haipv <haipv@evolableasia.vn>
2019-11-01 15:34:31 +0700

    init implatement provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'application.dart';
-import 'trans/global_translations.dart';
+import 'service/languages_service.dart';
 
 void main() async {
-
-  // init translation base on preferred
-  await allTranslations.init();
-
   runApp(Application());
 }
 
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'new app',
+      theme: ThemeData(primarySwatch: Colors.red),
+      localizationsDelegates: [
+        const TranslationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('vi', ''),
+      ],
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(Translations.of(context).text('hello')),
+              RaisedButton(
+                child: Text((Translations.of(context).text('change'))),
+                onPressed: changeLang,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
+  void changeLang() {
+  }
+}
---
+++ lib/setup_injection.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+
+import 'service/languages_service.dart';
+
+
+GetIt locator = GetIt.instance;
+
+void setupLocator(){
+//    locator.registerLazySingleton(() => LanguageService());
+}
